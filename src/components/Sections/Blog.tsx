33c78ec3dea877a5f0e1639aca5a5bf4a6c8133f
m.raziei <mohammadraziei1375@gmail.com>
2023-12-18 08:37:42 +0330

    adding dancing like

--- src/components/Sections/Blog.tsx
+++ src/components/Sections/Blog.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import Link from 'next/link';
 import {FC, memo} from 'react';
 
@@ -5,15 +6,21 @@ import {SectionId} from '../../data/data';
 import ReactLogo from '../../images/logo.svg';
 import Section from '../Layout/Section';
 
+const DynamicDancingLines = dynamic(() => import('react-dancing-lines'), {
+  ssr: false, // Prevents server-side rendering of the component
+});
+
 const Blog: FC = memo(() => {
   return (
     <Section sectionId={SectionId.Blog}>
+      <DynamicDancingLines />
+
       <div className="flex flex-col items-center text-center text-neutral-300">
-        <Link href="https://mohammadraziei.github.io/blog">
+        <Link href="https://mohammadraziei.github.io/blog" target="_blank">
           <ReactLogo className="h-32 m-8 mb-12" />
         </Link>
         <h1 className="text-4xl font-bold justify-center">Visit my weblog to know more about me </h1>
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-2">
           <Link
             className="text-xl font-bold m-4 hover:text-gray-400"
             href="https://mohammadraziei.github.io/blog/blog"
